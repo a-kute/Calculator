8b90e95b54a61c87dc5ed0911e3a1ce2c99f7f94
atharva <akute@purdue.edu>
2021-01-12 18:57:26 -0500

    Optimized memory and allowed for multiple calculations

--- main.c
+++ main.c
@@ -16,27 +16,50 @@ int main() {
     //Welcome
     printf("Welcome to the Calculator Application!\nPlease enter a valid expression to evaluate!\n");
 
-    //take input
     char input[20];
+
+
+    char* pinput = &input;
+
+
     int firstInput;
     char operator;
     int secondInput;
+    int counter = 0;
+
     start: do {
-        fgets(input, sizeof(input), stdin);
-        if (strstr(input," ") == NULL) {
+
+        pinput = calloc(1, sizeof(input));
+
+        if (counter != 0) {
+            printf("Enter an Expression\n");
+            pinput = calloc(1, (char) 2);
+            fgets(pinput,2,stdin);
+            free(pinput);
+            pinput = calloc(1, sizeof(input));
+
+        }
+        counter++;
+
+        fgets(pinput, 21, stdin);
+        if (strstr(pinput," ") == NULL) {
             printf("Please enter valid expression with spaces!\n");
             goto start;
         }
-        firstInput = atoi(strtok(input," "));
+        firstInput = atoi(strtok(pinput," "));
         char* modifier = strtok(NULL," ");
         operator = modifier[0];
         secondInput = atoi(strtok(NULL," "));
         printf("%d%c%d = %d\n",firstInput,operator,secondInput,calculate(firstInput,secondInput,operator));
+        free(pinput);
+        pinput = calloc(1, (char) 4);
         printf("Do you want to enter another equation?\n");
-        fgets(input, sizeof(input), stdin);
-    } while (strcmp(input,"yes") == 0);
-    //Exiting
-    printf("Now exiting the application");
+        fgets(pinput, 4, stdin);
+
+
+    } while (strcmp(pinput,"yes") == 0);
+        //Exiting
+        printf("Now exiting the application");
 }
 
 int calculate(int a, int b, char c) {
